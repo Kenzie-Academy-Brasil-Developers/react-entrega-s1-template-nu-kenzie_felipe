1f0483485c2e1fa2b46e6a6a19070f209c7b1679
felipe <fdomingueskt@gmail.com>
2023-01-27 12:51:03 -0300

    finalizando desktop

---
+++ nukenzie/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import './App.css'
+import backgroundwhite from './assets/backgroundwhitemobile.svg'
+import Dash from './assets/components/dashboard'
+import './global.css'
+
+
+function App() {
+  const [isPage, setIsPage] = useState(false)
+  return (
+    <>
+   <div className="homePage">
+    {isPage ? < Dash /> : 
+    <div>
+    <header className='Side-Top'>
+    <h4 className="title-Nu">Nu</h4><h4 className="title-Kenzie">Kenzie</h4>
+        <p className='description1'>Centralize o controle das suas finanças</p>
+        <p className='quick'>de forma rápida e segura</p>
+        <button className='Start' type='button' onClick={() => setIsPage(true)}>Iniciar</button>
+    </header>
+      <main>
+      <img  src={backgroundwhite} className='background-white'></img>
+      </main>
+    </div>
+    }
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ nukenzie/src/assets/components/dashboard.jsx
@@ -0,0 +1,102 @@
+import { useState } from 'react'
+import './dashboard.css'
+import '/src/global.css'
+import App from '../../App'
+
+
+
+function Dash() {
+    const [isPage, setIsPage] = useState(false)
+
+    const [Text, setText] = useState("")
+    const [value, setValue] = useState("")
+    const [type, setType] = useState("")
+    const [list, setList] = useState([]);
+    const totalValue = list.reduce((acc, item) => acc + parseFloat(item.value), 0);
+
+    const handleDelete = (index) => {
+        const updatedList = list.filter((item, i) => i !== index);
+        setList(updatedList);
+    }
+
+
+
+    return (
+        <>
+            {isPage ? < App /> :
+                <div className='container'>
+                    <header className="header">
+                        <h1 className='title-nu'>Nu</h1><h1 className='title-kenzie'>Kenzie</h1>
+                        <button type='button' className='logout' onClick={() => setIsPage(true)}>Inicio</button>
+                    </header>
+                    <main>
+                        <div className="form">
+                            <form>
+                                <p className='description'>Descrição</p>
+                                <input placeholder="Digite aqui sua descrição" className='input' value={Text} onChange={e => setText(e.target.value)} />
+                                <p className='ex'>Ex: Compra de roupas</p>
+                                <p className='value'>Valor</p>
+                                <input placeholder="1" className='input-value' type='number' value={value} onChange={e => setValue(e.target.value)} />
+                                <p className='RS'>R$</p>
+
+
+                                <p className='type-name'>Tipo de valor</p>
+                                <select value={type} onChange={(e) => setType(e.target.value)}>
+                                    <option>Entrada</option>
+                                    <option>Despesa</option>
+                                </select>
+                            </form>
+
+                            <button type="button" className='set-value' onClick={() => setList([...list, { Text, value, type }])}>Inserir valor</button>
+                        </div>
+
+                        <div className="list">
+                            <div className="list-total">
+                                <p className="valor-total">Valor total:</p>
+                                <p className="money">$ {totalValue}</p>
+                                <p className='Ref'>O valor referente ao saldo</p>
+                            </div>
+                            <h5>Resumo financeiro</h5>
+
+                            <h1 className={`title-before ${list.length > 0 ? 'hidden' : ''}`}>Você ainda não possui nenhum lançamento</h1>
+
+
+                            {/* <h1 className='title-before hidden'>Você ainda não possui nenhum lançamento</h1> */}
+
+
+
+
+
+
+                            <div className='list-click'>
+                                <ul className='list-scroll'>
+                                    {list.map((item, index) => (
+                                        <li key={index}>
+                                            <div className="set-click">
+                                                <h4 className="click-title">{item.Text}</h4>
+                                                <p className="click-type">{item.type}</p>
+                                                <p className="click-money">R$ {item.value}</p>
+                                                <button type="button" className='trash-btn' onClick={() => handleDelete(index)}><img src='/src/assets/trash.svg' className='trash'></img>
+                                                 </button>
+
+                                            </div>
+                                        </li>
+                                    ))}
+                                </ul>
+                                
+                            </div>
+
+
+
+                        </div>
+
+                    </main>
+
+                </div>
+            }
+
+        </>
+    )
+}
+
+export default Dash
