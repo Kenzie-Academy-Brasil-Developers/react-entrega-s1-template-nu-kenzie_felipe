4c36a24e834142822aab0159791015bfae027bef
felipe <fdomingueskt@gmail.com>
2023-02-14 15:45:08 -0300

    Arrumando responsividade

--- nukenzie/src/components/dashboard.jsx
+++ nukenzie/src/components/dashboard.jsx
@@ -34,90 +34,94 @@ const Dash = () => {
             {isPage ? < App /> :
 
                 <div className='resolved-background'>
+                    <header className='header'>
+                        <div className='container'>
+                        <div className='display-flex'>
 
-                    <div className='container'>
-                        <header className='header'>
-                            <div className='display-flex'>
-
-                            <div className='titles-dash'>  <h1 className='title-nu'>Nu</h1><h1 className='title-kenzie'>Kenzie</h1> </div>
+<div className='titles-dash'>  <h1 className='title-nu'>Nu</h1><h1 className='title-kenzie'>Kenzie</h1> </div>
 
 <button type='button' className='logout' onClick={() => setIsPage(true)}>Inicio</button>
-                            </div>
-                            
-                        </header>
-                        <main>
-                            <div className='Two-sides'>
+</div>
+                        </div>
+                        
+
+                    </header>
+                    <main>
+                    <div className='container'>
+                        <div className='Two-sides'>
                             <div className="form">
                                 <form>
                                     <div className='form-top'>
-                                    <p className='description'>Descrição</p>
-                                    <input placeholder="Digite aqui sua descrição" className='input' value={Text} onChange={(e) => setText(e.target.value)} />
-                                    <p className='ex'>Ex: Compra de roupas</p>
+                                        <p className='description'>Descrição</p>
+                                        <input placeholder="Digite aqui sua descrição" className='input' value={Text} onChange={(e) => setText(e.target.value)} />
+                                        <p className='ex'>Ex: Compra de roupas</p>
                                     </div>
-                                  
+
                                     <div className='form-body'>
-                                    <p className='value'>Valor</p>
-                                    <input placeholder="1" className='input-value' type='number' value={value} onChange={(e) => setValue(e.target.value)} />
-                                    <p className='RS'>R$</p>    
+                                        <p className='value'>Valor</p>
+                                        <input placeholder="1" className='input-value' type='number' value={value} onChange={(e) => setValue(e.target.value)} />
+                                        <p className='RS'>R$</p>
 
                                     </div>
-                                    
+
 
 
                                     <div className='form-footer'>
-                                    <p className='type-name'>Tipo de valor</p>
-                                    <select value={type} onChange={(e) => setType(e.target.value)}>
-                                        <option>Selecione</option>
-                                        <option>Entrada</option>
-                                        <option>Despesa</option>
-                                    </select>
+                                        <p className='type-name'>Tipo de valor</p>
+                                        <select value={type} onChange={(e) => setType(e.target.value)}>
+                                            <option>Selecione</option>
+                                            <option>Entrada</option>
+                                            <option>Despesa</option>
+                                        </select>
                                     </div>
                                     <button type='button' className='set-value' onClick={() => setList([...list, { Text, value, type }])}>Inserir valor</button>
                                 </form>
 
-                                
-                            </div>
-                            
-                 
-
-                            <div className='list'>
+                                <div className='list'>
                                 <div className='list-total'>
                                     <p className='value-total'>Valor total:</p>
                                     <p className='money'>$ {totalValue}</p>
                                     <p className='Ref'>O valor referente ao saldo</p>
                                 </div>
-                                </div>
-                                <aside className='resolved-response'>        
-                                       <h5>Resumo financeiro</h5>
-
-                                    <h1 className={`title-before ${list.length > 0 ? 'hidden' : ''}`}>Você ainda não possui nenhum lançamento</h1>
-                                </aside>
-
-                                <div className='list-click'>
-                                    <ul className='list-scroll'>
-                                        {list.map((item, index) => (
-                                            <li key={index}>
-                                                <div className='set-click'>
-                                                    <h4 className='click-title'>{item.Text}</h4>
-                                                    <p className='click-type'>{item.type}</p>
-                                                    <p className='click-money'>R$ {item.value}</p>
-                                                    <button type='button' className='trash-btn' onClick={() => handleDelete(index)}><img src={Trash} className='trash'></img>
-                                                    </button>
-
-                                                </div>
-                                            </li>
-                                        ))}
-                                    </ul>
-
-                                </div>
-
-
-
+                            </div>
                             </div>
 
-                        </main>
 
-                    </div>
+
+                           
+                            <aside className='resolved-response'>
+                                <h5>Resumo financeiro</h5>
+
+                                <h1 className={`title-before ${list.length > 0 ? 'hidden' : ''}`}>Você ainda não possui nenhum lançamento</h1>
+
+                                <div className='list-click'>
+                                <ul className='list-scroll'>
+                                    {list.map((item, index) => (
+                                        <li key={index}>
+                                            <div className='set-click'>
+                                                <h4 className='click-title'>{item.Text}</h4>
+                                                <p className='click-type'>{item.type}</p>
+                                                <p className='click-money'>R$ {item.value}</p>
+                                                <button type='button' className='trash-btn' onClick={() => handleDelete(index)}><img src={Trash} className='trash'></img>
+                                                </button>
+
+                                            </div>
+                                        </li>
+                                    ))}
+                                </ul>
+
+                            </div>
+                            </aside>
+
+                         
+
+
+
+                        </div>
+                     </div>                       
+                    </main>
+
+
 
                 </div>
 
